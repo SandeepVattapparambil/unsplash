68044139cd9b7145662a1ea3e4c7f54db120fcad
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-01-30 13:47:45 +0530

    webpack server build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const React = require('react');
-const renderTtoString = require('react-dom/server').renderTtoString;
+const renderToString = require('react-dom/server').renderToString;
 const Home = require('./client/components/Home').default;
 const app = express();
 
 app.get('/', (req, res) => {
-const content = renderTtoString(<Home />);
-res.send(content);
+    const content = renderToString( < Home / > );
+    res.send(content);
 });
 app.listen(3000, () => {
     console.log('App listening on port 3000');
