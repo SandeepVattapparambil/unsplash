eb7a0fa58b1f6b8a5bfc484401bc0074c3b38f06
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-01-30 13:36:59 +0530

    webpack updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const React = require('react');
+const renderTtoString = require('react-dom/server').renderTtoString;
+const Home = require('./client/components/Home').default;
 const app = express();
 
 app.get('/', (req, res) => {
-
+const content = renderTtoString(<Home />);
+res.send(content);
 });
 app.listen(3000, () => {
     console.log('App listening on port 3000');
